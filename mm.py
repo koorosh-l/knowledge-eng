e71bfc0c4a3c902ed78916a6c0e4d802da03d6df
koorosh <koorosh.akl@protonmail.com>
2024-11-03 16:48:06 +0300

    minimum of distance matrix

--- mm.py
+++ mm.py
@@ -1,6 +1,7 @@
 import math
 from pprint import pprint
-
+def f2str (f):
+    return "{:.1f}".format(f)
 def eu_dist(v1, v2):
     res = 0
     for i in  range(len(v1)):
@@ -22,3 +23,14 @@ def matrix_profile(vec, n):
         res.append(vector_profile(vec, sub_arr))
         # print(sub_arr)
     return res
+def minvec(mat):
+    res = []
+    for rows in mat:
+        min = float("inf")
+        for i in rows:
+            if i != 0 and i < min:
+                min = i
+        res.append(min)
+    return res
+#for i in minvec(matrix_profile([0, 1, 3, 2, 9, 1, 14, 15, 1, 2, 2, 10, 7], 4)):
+#    print(f2str(i))
